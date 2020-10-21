edac837bf6ec10d64bae5a0754a14abdaf91da55
Anderson de Sa <asilvadesa@gmail.com>
2020-10-20 23:10:46 -0400

    Iniciando Appium Client

--- src/main/java/br/com/asilva/appium/App.java
+++ src/main/java/br/com/asilva/appium/App.java
@@ -1,13 +1,29 @@
 package br.com.asilva.appium;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws MalformedURLException {
+
+        File apk = new File("/home/anderson/IdeaProjects/appium-test/src/main/resources/alura_esporte.apk");
+        DesiredCapabilities configuracoes =  new DesiredCapabilities();
+        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        URL urlConexao = new URL("http://127.0.0.1:4723/wd/hub");
+        AppiumDriver driver = new AppiumDriver<>(urlConexao, configuracoes);
     }
 }
