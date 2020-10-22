5ed85015060af066b4805bf3db3d40b83d078633
Anderson de Sa <asilvadesa@gmail.com>
2020-10-22 17:58:21 -0400

    Adicionado caso de teste para senham que conferem

--- src/test/java/br/com/asilva/appium/CadastroTest.java
+++ src/test/java/br/com/asilva/appium/CadastroTest.java
@@ -2,11 +2,13 @@ package br.com.asilva.appium;
 
 import static org.junit.Assert.assertTrue;
 
+import io.appium.java_client.MobileElement;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
 
@@ -24,7 +26,20 @@ public class CadastroTest
         driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar")).click();
         String text = driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/erro_cadastro")).getText();
         Assert.assertNotNull(text);
+    }
 
+    @Test
+    public void deve_CadastrarUsuariosComSenhaQueConferem() throws MalformedURLException {
+        AppiumDriverConfig driver = new AppiumDriverConfig();
+        MobileElement botaoCadastro = (MobileElement) driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/login_botao_cadastrar_usuario"));
+        botaoCadastro.click();
+        driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/input_nome")).sendKeys("asilvadesa");
+        driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/input_senha")).sendKeys("123");
+        driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/input_confirmar_senha")).sendKeys("123");
+        driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/cadastro_usuario_botao_cadastrar")).click();
+
+        MobileElement botaoLogar = (MobileElement) driver.driver.findElement(By.id("br.com.alura.aluraesporte:id/login_botao_logar"));
+        Assert.assertNotNull(botaoLogar);
     }
 
 }
